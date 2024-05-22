01d80975023f547e42fe31a188e3cd945a335303
thecooolemail <thecooolemail@gmail.com>
2024-05-22 14:58:22 +0100

    Angular

--- src/ui/routes.ts
+++ src/ui/routes.ts
@@ -1,11 +1,14 @@
+import { Route } from '@angular/router';
 import { registerRouteComponent } from "@vendure/admin-ui/core";
 import { GreeterComponent } from "./fulfillments.component";
 
-export default [
+const routes: Route[] = [
     registerRouteComponent({
         component: GreeterComponent,
         path: '',
         title: '',
         breadcrumb: 'fulfillments',
     }),
-]
+];
+
+export default routes;
